d7e11de54df3b6e99687cd14d54f4e497cdf1ea9
Joshua-Shockley <thelumbashockley@gmail.com>
2019-08-13 11:00:13 -0500

    added usestate and useeffect and jsx type to vscode

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
